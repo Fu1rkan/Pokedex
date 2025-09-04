a0db8d475af5e2fcf1d2c00ddc8ac6b7959a9f66
Fu1rkan <furkanyigit2000@gmail.com>
2025-09-04 07:58:56 +0200

    search function works now

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 let pokemonCounter = 0;
 
+let nameCounter = 1;
+
+let names = ['Furkan'];
+
 let colorByType = {
     "normal":   "#A8A77A",
     "fire":     "#EE8130",
@@ -22,7 +26,6 @@ let colorByType = {
 }
 
 async function loadPokemon() {
-    JSON.parse(localStorage.getItem("pokemon"));
     try{
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
         let data = await response.json();
@@ -40,7 +43,9 @@ async function generatePokemons(pokemonList){
         let pokemonStats = await fetch(pokemonList[i].url);
         let pokemonData = await pokemonStats.json();
         await setToHtml(pokemonData);
+        names.push(pokemonData.name);
     }
+    nameCounter += 20;    
 }
 
 async function setToHtml(pokemonData){
@@ -59,10 +64,10 @@ async function checkType(pokemonData, typeImg, species) {
         let type2Data = await fetch(pokemonData.types[1].type.url);
         let type2 = await type2Data.json();
         let type2Img = type2.sprites['generation-viii']['sword-shield'].name_icon
-        document.getElementById('pokemon-cards-overlay').innerHTML += pokemonOverlay2Types(pokemonData, typeImg, type2Img);
-        document.getElementById('main-pokemon-overlay').innerHTML += pokemonMainOverlay2Types(pokemonData, species, typeImg, type2Img)
+        document.getElementById('pokemon-cards-overlay').innerHTML += pokemonOverlayTypes(pokemonData, typeImg, type2Img);
+        document.getElementById('main-pokemon-overlay').innerHTML += pokemonMainOverlay(pokemonData, species, typeImg, type2Img)
     }else{
-        document.getElementById('pokemon-cards-overlay').innerHTML += pokemonOverlay(pokemonData, typeImg);
+        document.getElementById('pokemon-cards-overlay').innerHTML += pokemonOverlayTypes(pokemonData, typeImg);
         document.getElementById('main-pokemon-overlay').innerHTML += pokemonMainOverlay(pokemonData, species, typeImg)
     }
 }
@@ -70,20 +75,20 @@ async function checkType(pokemonData, typeImg, species) {
 async function checkEvoChain(pokemonData, pokemon){
     let response = await fetch(pokemon.evolution_chain.url);
     let data = await response.json();
-    let evoChain = data.chain.evolves_to;
     
-    if (evoChain.length > 0) {
-        if (evoChain[0].evolves_to.length > 0) {
-            if (evoChain[0].evolution_details.length > 0) {
-                let evoChain1Response = await fetch(data.chain.species.url);
-                let evoChain1 = await evoChain1Response.json();
-                let pokemonData1Response = await fetch(evoChain1.varieties[0].pokemon.url);
-                let pokemonData1 = await pokemonData1Response.json();
-    
-                let evoChain2Response = await fetch(data.chain.evolves_to[0].species.url);
-                let evoChain2 = await evoChain2Response.json();
-                let pokemonData2Response = await fetch(evoChain2.varieties[0].pokemon.url);
-                let pokemonData2 = await pokemonData2Response.json();
+    if (data.chain.evolves_to.length > 0) {
+        let evoChain1Response = await fetch(data.chain.species.url);
+        let evoChain1 = await evoChain1Response.json();
+        let pokemonData1Response = await fetch(evoChain1.varieties[0].pokemon.url);
+        let pokemonData1 = await pokemonData1Response.json();
+
+        let evoChain2Response = await fetch(data.chain.evolves_to[0].species.url);
+        let evoChain2 = await evoChain2Response.json();
+        let pokemonData2Response = await fetch(evoChain2.varieties[0].pokemon.url);
+        let pokemonData2 = await pokemonData2Response.json();
+
+        if (data.chain.evolves_to[0].evolves_to.length > 0) {
+            if (data.chain.evolves_to[0].evolution_details.length > 0) {
     
                 let evoChain3Response = await fetch(data.chain.evolves_to[0].evolves_to[0].species.url);
                 let evoChain3 = await evoChain3Response.json();
@@ -91,21 +96,8 @@ async function checkEvoChain(pokemonData, pokemon){
                 let pokemonData3 = await pokemonData3Response.json();
 
                 document.getElementById(`evo-chain-overlay${pokemonData.id}`).innerHTML += twoEvoChainOverlay(pokemonData1, pokemonData2, pokemonData3);
-            }else{
-                console.log('hat geklappt');
-                
             }
         }else{
-            let evoChain1Response = await fetch(data.chain.species.url);
-            let evoChain1 = await evoChain1Response.json();
-            let pokemonData1Response = await fetch(evoChain1.varieties[0].pokemon.url);
-            let pokemonData1 = await pokemonData1Response.json();
-
-            let evoChain2Response = await fetch(data.chain.evolves_to[0].species.url);
-            let evoChain2 = await evoChain2Response.json();
-            let pokemonData2Response = await fetch(evoChain2.varieties[0].pokemon.url);
-            let pokemonData2 = await pokemonData2Response.json();
-
             document.getElementById(`evo-chain-overlay${pokemonData.id}`).innerHTML += oneEvoChainOverlay(pokemonData1, pokemonData2);
         }
         
@@ -154,3 +146,23 @@ async function playAudio(pokemon){
     let data = await response.json();
     new Audio(data.cries.latest).play();
 }
+
+function searchPokemon(){
+    let input = document.getElementById('search-bar').value;
+    input = input.toLowerCase();
+
+    for (let i = 1; i < names.length; i++) {
+        if (input.length >=3) {
+            if (!names[i].toLowerCase().includes(input)) {
+                console.log('kein Ergebnis');
+                document.getElementById(`${names[i]}`).classList.add('d_none');
+            }else {
+                document.getElementById(`${names[i]}`).classList.remove('d_none');
+            }
+        }else{
+            document.getElementById(`${names[i]}`).classList.remove('d_none');
+        }
+    }
+}
+
+document.getElementById("search-bar").addEventListener("input", searchPokemon);
--- script/template.js
+++ script/template.js
@@ -1,6 +1,6 @@
-function pokemonOverlay(pokemonData, typeImg){
+function pokemonOverlayTypes(pokemonData, typeImg, type2Img){
     return `
-        <div class="pokemon_card" onclick="toggleRespPokemonOverlay(${pokemonData.id})" style="background-color: ${colorByType[pokemonData.types[0].type.name]}">
+        <div class="pokemon_card" onclick="toggleRespPokemonOverlay(${pokemonData.id})" style="background-color: ${colorByType[pokemonData.types[0].type.name]}" id="${pokemonData.name}">
             <div class="name_and_id">
                 <h3 id="pokemon-name">${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h3>
                 <p>#${pokemonData.id}</p>
@@ -9,37 +9,33 @@ function pokemonOverlay(pokemonData, typeImg){
                 <img src="${pokemonData.sprites.other['official-artwork'].front_default}" alt="" class="pokemon_img">
             </div>
             <div class="pokemon_type">
-                <div class="pokemon_type_container">
-                    <img src="${typeImg}" alt="">
-                </div>
+                ${type(typeImg, type2Img, pokemonData)}
             </div>
         </div>
     `
 }
 
-function pokemonOverlay2Types(pokemonData, typeImg, type2Img){
-    return `
-        <div class="pokemon_card" onclick="toggleRespPokemonOverlay(${pokemonData.id})" style="background-color: ${colorByType[pokemonData.types[0].type.name]}">
-            <div class="name_and_id">
-                <h3 id="pokemon-name">${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h3>
-                <p>#${pokemonData.id}</p>
+function type(typeImg, type2Img, pokemonData){
+    if (pokemonData.types.length > 1) {
+        return `
+            <div class="pokemon_type_container">
+                <img src="${typeImg}" alt="">
             </div>
-            <div>
-                <img src="${pokemonData.sprites.other['official-artwork'].front_default}" alt="" class="pokemon_img">
+            <div class="pokemon_type_container">
+                <img src="${type2Img}" alt="">
             </div>
-            <div class="pokemon_type">
-                <div class="pokemon_type_container">
-                    <img src="${typeImg}" alt="">
-                </div>
-                <div class="pokemon_type_container">
-                    <img src="${type2Img}" alt="">
-                </div>
+        `
+    }else{
+        return `
+            <div class="pokemon_type_container">
+                <img src="${typeImg}" alt="">
             </div>
-        </div>
-    `
+        `
+    }
+    
 }
 
-function pokemonMainOverlay(pokemonData, species, typeImg){
+function pokemonMainOverlay(pokemonData, species, typeImg, typeImg2){
     return `
         <img src="./img/left-arrow.png" alt="" class="d_none switch_arrows" id="switch-arrow${pokemonData.id}" onclick="switchBackward(${pokemonData.id})">
         <div class="resp_pokemon_overlay d_none" id="pokemon${pokemonData.id}">
@@ -53,7 +49,7 @@ function pokemonMainOverlay(pokemonData, species, typeImg){
                 <img src="./img/pokeball.png" alt="" class="pokeball_bg">
             </div>
             <div class="pokemon_type_resp_overlay">
-                <img src="${typeImg}" alt="">
+                ${overlayType(pokemonData,typeImg, typeImg2)}
             </div>
             <div class="pokemon_info_bar">
                 <div class="pokemon_info_type pokemon_info_type_border" onclick="setActiveOverlay('info-about${pokemonData.id}', 'about-overlay${pokemonData.id}', ${pokemonData.id})" id="info-about${pokemonData.id}">about</div>
@@ -149,115 +145,17 @@ function pokemonMainOverlay(pokemonData, species, typeImg){
     `
 }
 
-function pokemonMainOverlay2Types(pokemonData, species, typeImg, type2Img){
-    return `
-        <img src="./img/left-arrow.png" alt="" class="d_none switch_arrows" id="switch-arrow${pokemonData.id}" onclick="switchBackward(${pokemonData.id})">
-        <div class="resp_pokemon_overlay d_none" id="pokemon${pokemonData.id}">
-            <div class="name_and_id_resp_overlay">
-                <img src="./img/close.png" alt="" onclick="toggleRespPokemonOverlay(${pokemonData.id})" class="close_resp_overlay">
-                <h2>${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h2>
-                <p>#${pokemonData.id}</p>
-            </div>
-            <div class="pokemon_img_container_resp_overlay" style="background-image: linear-gradient(90deg, ${colorByType[pokemonData.types[0].type.name]} 10%, ${colorByType[pokemonData.types[1].type.name]} 70%);">
-                <img src="${pokemonData.sprites.other['official-artwork'].front_default}" class="pokemon_img_resp_overlay" onclick="playAudio(${pokemonData.id})">
-                <img src="./img/pokeball.png" alt="" class="pokeball_bg">
-            </div>
-            <div class="pokemon_type_resp_overlay">
-                <img src="${typeImg}" alt="">
-                <img src="${type2Img}" alt="">
-            </div>
-            <div class="pokemon_info_bar">
-                <div class="pokemon_info_type pokemon_info_type_border" onclick="setActiveOverlay('info-about${pokemonData.id}', 'about-overlay${pokemonData.id}' ,${pokemonData.id})" id="info-about${pokemonData.id}">about</div>
-                <div class="pokemon_info_type" onclick="setActiveOverlay('info-stats${pokemonData.id}', 'stats-overlay${pokemonData.id}' ,${pokemonData.id})" id="info-stats${pokemonData.id}">stats</div>
-                <div class="pokemon_info_type" onclick="setActiveOverlay('info-evo-chain${pokemonData.id}', 'evo-chain-overlay${pokemonData.id}' ,${pokemonData.id})" id="info-evo-chain${pokemonData.id}">evo chain</div>
-            </div>
-            <div class="about_overlay" id="about-overlay${pokemonData.id}">
-                <table class="table_info">
-                    <tr>
-                        <td>Species</td>
-                        <td class="t_bold">${species.genera[7].genus}</td>
-                    </tr>
-                    <tr>
-                        <td>Height</td>
-                        <td class="t_bold">${pokemonData.height}m</td>
-                    </tr>
-                    <tr>
-                        <td>Wheight</td>
-                        <td class="t_bold">${pokemonData.weight}kg</td>
-                    </tr>
-                    <tr>
-                        <td>Base Experience</td>
-                        <td class="t_bold">${pokemonData.base_experience}</td>
-                    </tr>
-                </table>
-            </div>
-            <div class="stats_overlay d_none" id="stats-overlay${pokemonData.id}">
-                <table class="stats_table">
-                    <tr>
-                        <td class="stats">hp</td>
-                        <td class="stats_percentage">${pokemonData.stats[0].base_stat}</td>
-                        <td class="stats_bar_td">
-                            <div class="stats_bar_container">
-                                <div class="stats_bar" style="width: calc(100%*${pokemonData.stats[0].base_stat}/255)"></div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="stats">attack</td>
-                        <td class="stats_percentage">${pokemonData.stats[1].base_stat}</td>
-                        <td class="stats_bar_td">
-                            <div class="stats_bar_container">
-                                <div class="stats_bar" style="width: calc(100%*${pokemonData.stats[1].base_stat}/190)"></div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="stats">defense</td>
-                        <td class="stats_percentage">${pokemonData.stats[2].base_stat}</td>
-                        <td class="stats_bar_td">
-                            <div class="stats_bar_container">
-                                <div class="stats_bar" style="width: calc(100%*${pokemonData.stats[2].base_stat}/250)"></div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="stats">special-attack</td>
-                        <td class="stats_percentage">${pokemonData.stats[3].base_stat}</td>
-                        <td class="stats_bar_td">
-                            <div class="stats_bar_container">
-                                <div class="stats_bar" style="width: calc(100%*${pokemonData.stats[3].base_stat}/194)"></div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="stats">special-defense</td>
-                        <td class="stats_percentage">${pokemonData.stats[4].base_stat}</td>
-                        <td class="stats_bar_td">
-                            <div class="stats_bar_container">
-                                <div class="stats_bar" style="width: calc(100%*${pokemonData.stats[4].base_stat}/250)"></div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="stats">speed</td>
-                        <td class="stats_percentage">${pokemonData.stats[5].base_stat}</td>
-                        <td class="stats_bar_td">
-                            <div class="stats_bar_container">
-                                <div class="stats_bar" style="width: calc(100%*${pokemonData.stats[5].base_stat}/200)"></div>
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div class="evo_chain_overlay d_none" id="evo-chain-overlay${pokemonData.id}">
-            </div>
-        </div>
-        <img src="./img/right-arrow.png" alt="" class="d_none switch_arrows" id="switch-arrow-right${pokemonData.id}" onclick="switchForward(${pokemonData.id})">
-        <div class="resp_arrows">
-            <img src="./img/left-arrow.png" alt="" class="d_none switch_arrows_resp" id="switch-arrow-resp${pokemonData.id}" onclick="switchBackward(${pokemonData.id})">
-            <img src="./img/right-arrow.png" alt="" class="d_none switch_arrows_resp" id="switch-arrow-right-resp${pokemonData.id}" onclick="switchForward(${pokemonData.id})">
-        </div>
-    `
+function overlayType(pokemonData, typeImg, typeImg2){
+    if (pokemonData.types.length > 1) {
+        return `
+            <img src="${typeImg}" alt="">
+            <img src="${typeImg2}" alt="">
+        `
+    }else{
+        return `
+            <img src="${typeImg}" alt="">
+        `
+    }
 }
 
 function noEvoChainOverlay(){
